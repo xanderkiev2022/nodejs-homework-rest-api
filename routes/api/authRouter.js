264e9273d5cec60342939288a343a1fad2974fe5
Alex Koval <113243861+xanderkiev2022@users.noreply.github.com>
2023-04-02 15:24:35 +0300

    Revert "Hw05 avatars"

--- routes/api/authRouter.js
+++ routes/api/authRouter.js
@@ -1,17 +1,15 @@
 const express = require("express");
 const router = express.Router();
-
 const ctrlAuth = require("../../src/controllers/authController");
 const { asyncWrapper } = require("../../src/helpers/apiHelpers");
 const { validation, register, login, subscription } = require("../../src/middlewares/validationMiddleware");
 const { authMiddleware } = require("../../src/middlewares/authMiddleware");
-const { uploadMiddleware } = require("../../src/middlewares/uploadMiddleware");
 
 router.post("/register", validation(register), asyncWrapper(ctrlAuth.registration));
 router.post("/login", validation(login), asyncWrapper(ctrlAuth.login));
 router.post("/logout", authMiddleware, asyncWrapper(ctrlAuth.logout));
 router.post("/current", authMiddleware, asyncWrapper(ctrlAuth.current));
 router.patch("/", authMiddleware, validation(subscription), asyncWrapper(ctrlAuth.updateSubscription));
-router.patch("/avatars", authMiddleware, uploadMiddleware.single('avatar'), asyncWrapper(ctrlAuth.updateAvatar));
+
 
 module.exports = router;
--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,4 @@
 const serviceAuth = require("../../src/services/authService");
-const { ValidationError } = require("../helpers/errors");
 
 const registration = async (req, res, next) => {
   const data = req.body;
@@ -10,7 +9,7 @@ const registration = async (req, res, next) => {
 const login = async (req, res, next) => {
   const data = req.body;
   const result = await serviceAuth.login(data);
-  res.status(200).json({ message: "Success login", token: result.token, user: { email: result.email, subscription: result.subscription },});
+  res.status(200).json({ message: "Success login", token: result.token, user: { eamil: result.email, subscription: result.subscription },});
 };
 
 const logout = async (req, res, next) => {
@@ -32,19 +31,10 @@ const updateSubscription = async (req, res, next) => {
   res.status(200).json({ message: "Subscription was updated", user: { result} });
 };
 
-const updateAvatar = async (req, res, next) => {
-  const { _id } = req.user;
-  const avatarData = req.file;
-  if (!avatarData) throw new ValidationError("Wrong type of image");  
-  const avatarURL = await serviceAuth.updateAvatar(_id, avatarData);
-  res.status(200).json({ message: "Avatar was updated", avatarURL });
-};
-
 module.exports = {
   registration,
   login,
   logout,
   current,
   updateSubscription,
-  updateAvatar,
 };
